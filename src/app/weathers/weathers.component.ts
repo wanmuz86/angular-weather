958b2cbc031220f0733db13ff1f0ff22a4505a1e
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2019-06-20 21:00:13 +0800

    first commit

---
+++ src/app/weathers/weathers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weathers',
+  templateUrl: './weathers.component.html',
+  styleUrls: ['./weathers.component.css']
+})
+export class WeathersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
